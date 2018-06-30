7a12ee4a385649bd6ecf3e6d3d1ed8e6527a1faf
Stefani Spayde <stefani@bearandgiraffe.com>
2018-06-30 02:41:54 -0500

    added author name attribute to comments table, registration form, and attached posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,6 @@ class Comment < ApplicationRecord
   validates :author_name, presence: true
   validates :body, presence: true, length: { maximum: 100 }
 
-def self.author_name
-  Comment.author_name
-end
+  attr_accessor :author_name
 
 end
